1980960b22ffa9edee063777668b1d9672a32841
shenghuang <1367467330@qq.com>
2019-06-17 01:51:52 +0800

    test1

---
+++ easyrpc/src/main/java/com/hs/easyrpc/common/Demo1.java
@@ -0,0 +1,8 @@
+package com.hs.easyrpc.common;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        // 测试项目结构
+        System.out.println("test project structure");
+    }
+}
