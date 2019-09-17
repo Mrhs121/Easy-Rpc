4e014d9d2521d5bcfb93ac58de3918c4f3445072
shenghuang <1367467330@qq.com>
2019-09-17 22:41:54 +0800

    add test

--- easyrpc/src/main/java/com/hs/easyrpc/demo/test/Test.java
+++ easyrpc/src/main/java/com/hs/easyrpc/demo/test/Test.java
@@ -9,16 +9,16 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("test");
-//        System.out.println("Easy-Rpc First Test!");
-//        HelloService service = RpcClient.getRemoteProxyObj(HelloService.class, new InetSocketAddress("localhost", 8088));
-//        for (int i=0;i<20;i++){
-//            System.out.println(service.Say(i+" huang sheng "));
-//        }
+        System.out.println("Easy-Rpc First Test!");
+        HelloService service = RpcClient.getRemoteProxyObj(HelloService.class, new InetSocketAddress("localhost", 8088));
+        for (int i=0;i<20;i++){
+            System.out.println(service.Say(i+" huang sheng "));
+        }
 
-        //        String json = JSON.toJSONString(request);
-//        System.out.println(json);
-//        RpcRequest q = JSON.parseObject(json,RpcRequest.class);
-//        System.out.println(q.toString());
+                String json = JSON.toJSONString(request);
+        System.out.println(json);
+        RpcRequest q = JSON.parseObject(json,RpcRequest.class);
+        System.out.println(q.toString());
 
     }
 }
